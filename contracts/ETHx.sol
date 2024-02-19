dd55862d3b7d10ff67985915966b118e85d95fda
muller <friedrichmueller012@gmail.com>
2024-02-19 15:06:37 +0800

    Update ETHx.sol

--- contracts/ETHx.sol
+++ contracts/ETHx.sol
@@ -5,6 +5,25 @@ import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol'
 import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
 
 
-contract ETHx {
+contract ETHx is Initializable, ERC20Upgradeable, PausableUpgradeable, AccessControlUpgradeable{
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant BURNER_ROLE = keccak256('BURNER_ROLE');
+    constructor() {
 
+    }
+    function initialize(address _admin) external initializer {
+        __ERC20_init('ETHx', 'ETHx');
+        __Pausable_init();
+        __AccessControl_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) whenNotPaused {
+        _mint(to, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) external onlyRole(BURNER_ROLE) whenNotPaused {
+        _burn(account, amount);
+    }
 }
