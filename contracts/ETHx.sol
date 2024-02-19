2a6db4c7c8101859bdddf7023b6138814387045b
muller <friedrichmueller012@gmail.com>
2024-02-19 15:05:03 +0800

    submitted

---
+++ contracts/ETHx.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
+
+
+contract ETHx {
+
+}
