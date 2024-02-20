a0591de215fc2cdb0e30c2396446b630e2b3bec9
muller <friedrichmueller012@gmail.com>
2024-02-20 12:54:20 +0800

    Create PermissionlessNodeRegistry.sol

---
+++ contracts/PermissionlessNodeRegistry.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
+
+
+contract PermissionlessNodeRegistry {
+
+}
