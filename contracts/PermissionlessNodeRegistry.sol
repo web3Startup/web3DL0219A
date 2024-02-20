8407e1631b729917698bdce22183ca7b622cd507
muller <friedrichmueller012@gmail.com>
2024-02-20 12:57:44 +0800

    Update PermissionlessNodeRegistry.sol

--- contracts/PermissionlessNodeRegistry.sol
+++ contracts/PermissionlessNodeRegistry.sol
@@ -1,11 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import './library/UtilLib.sol';
+import './library/ValidatorStatus.sol';
+import './interfaces/IStaderConfig.sol';
+import './interfaces/IVaultFactory.sol';
+import './interfaces/IPoolUtils.sol';
+import './interfaces/INodeRegistry.sol';
+import './interfaces/IPermissionlessPool.sol';
+import './interfaces/INodeELRewardVault.sol';
+import './interfaces/IStaderInsuranceFund.sol';
+import './interfaces/IValidatorWithdrawalVault.sol';
+import './interfaces/SDCollateral/ISDCollateral.sol';
+import './interfaces/IPermissionlessNodeRegistry.sol';
+import './interfaces/IOperatorRewardsCollector.sol';
+
 import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
 
-
-contract PermissionlessNodeRegistry {
+abstract contract PermissionlessNodeRegistry is
+    INodeRegistry,
+    IPermissionlessNodeRegistry,
+    AccessControlUpgradeable,
+    PausableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
 
 }
